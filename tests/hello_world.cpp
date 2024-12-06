880613c770cec5b97b061031c5c6859690e0c9fa
strandfield <vincent.chambrin@gmail.com>
2024-12-06 08:07:51 +0100

    refactored test hello world

--- tests/hello_world.cpp
+++ tests/hello_world.cpp
@@ -12,130 +12,100 @@
 
 using namespace cppscanner;
 
-TEST_CASE("hello_world compile_commands.json", "[scanner][hello_world]")
+TEST_CASE("hello_world test project", "[scanner][hello_world]")
 {
-  const std::string snapshot_name = "hello_world.db";
+  std::vector<std::string> snapshots;
 
-  ScannerInvocation inv{
-    { "--compile-commands", HELLO_WORLD_BUILD_DIR + std::string("/compile_commands.json"),
+  SECTION("compile_commands.json") {
+    const std::string snapshot_name = "hello_world-cc.db";
+
+    ScannerInvocation inv{
+      { "--compile-commands", HELLO_WORLD_BUILD_DIR + std::string("/compile_commands.json"),
       "--home", HELLO_WORLD_ROOT_DIR,
       "--overwrite",
       "-o", snapshot_name }
-  };
+    };
 
-  // the scanner invocation succeeds"
-  {
     REQUIRE_NOTHROW(inv.run());
-    REQUIRE(inv.errors().empty());
+    CHECK(inv.errors().empty());
+
+    if (inv.errors().empty()) {
+      snapshots.push_back(snapshot_name);
+    }
   }
 
-  SnapshotReader s{ snapshot_name };
+  SECTION("cmake") {
+    CMakeCommandInvocation cmake{
+      { "-B", HELLO_WORLD_BUILD_DIR,
+      "-S", HELLO_WORLD_ROOT_DIR }
+    };
 
-  std::vector<File> files = s.getFiles();
-  REQUIRE(files.size() >= 2);
-  File srcfile = getFile(files, std::regex("main\\.cpp"));
-  File hdrfile = getFile(files, std::regex("hello\\.h"));
-  File iostream = getFile(files, std::regex("iostream"));
+    cmake.exec();
 
-  // pp-includes are indexed correcly
-  {
-    std::vector<Include> includes = s.getIncludedFiles(srcfile.id);
-    REQUIRE(includes.size() == 1);
-    REQUIRE(includes.front().includedFileID == hdrfile.id);
+    CMakeIndex cmkindex;
+    REQUIRE(cmkindex.read(HELLO_WORLD_BUILD_DIR));
 
-    includes = s.getIncludedFiles(hdrfile.id);
-    REQUIRE(includes.size() == 1);
-    REQUIRE(includes.front().includedFileID == iostream.id);
-  }
-
-  SymbolRecord stdns = s.getSymbolByName("std");
-  REQUIRE(stdns.kind == SymbolKind::Namespace);
-  SymbolRecord stdcout = s.getSymbolByName("cout");
-  SymbolRecord stdendl = getRecord(s, SymbolRecordFilter().withNameLike("endl(%)").withParent(stdns.id));
-
-  // references to std symbols are correct
-  {
-    std::vector<SymbolReference> refs = s.findReferences(stdcout.id);
-    REQUIRE(!refs.empty());
-    filterRefs(refs, SymbolRefPattern(stdcout).inFile(hdrfile));
-    REQUIRE(refs.size() == 1);
-
-    refs = s.findReferences(stdendl.id);
-    REQUIRE(!refs.empty());
-    filterRefs(refs, SymbolRefPattern(stdendl).inFile(hdrfile));
-    REQUIRE(refs.size() == 1);
-  }
-}
-
-
-TEST_CASE("hello_world cmake", "[scanner][hello_world]")
-{
-  CMakeCommandInvocation cmake{
-    { "-B", HELLO_WORLD_BUILD_DIR,
-    "-S", HELLO_WORLD_ROOT_DIR }
-  };
-
-  cmake.exec();
-
-  CMakeIndex cmkindex;
-  REQUIRE(cmkindex.read(HELLO_WORLD_BUILD_DIR));
-
-  const std::string snapshot_name = "hello_world-cmake.db";
+    const std::string snapshot_name = "hello_world-cmake.db";
 
 #ifdef _WIN32
-  const std::string all = "ALL_BUILD";
+    const std::string all = "ALL_BUILD";
 #else
-  const std::string all = "all";
+    const std::string all = "all";
 #endif // _WIN32
 
-  ScannerInvocation inv{
-    { "--build", HELLO_WORLD_BUILD_DIR,
-    "--target", all,
-    "--home", HELLO_WORLD_ROOT_DIR,
-    "--overwrite",
-    "-o", snapshot_name }
-  };
+    ScannerInvocation inv{
+      { "--build", HELLO_WORLD_BUILD_DIR,
+      "--target", all,
+      "--home", HELLO_WORLD_ROOT_DIR,
+      "--overwrite",
+      "-o", snapshot_name }
+    };
 
-  // the scanner invocation succeeds"
-  {
     REQUIRE_NOTHROW(inv.run());
-    REQUIRE(inv.errors().empty());
+    CHECK(inv.errors().empty());
+
+    if (inv.errors().empty()) {
+      snapshots.push_back(snapshot_name);
+    }
   }
 
-  SnapshotReader s{ snapshot_name };
-
-  std::vector<File> files = s.getFiles();
-  REQUIRE(files.size() >= 2);
-  File srcfile = getFile(files, std::regex("main\\.cpp"));
-  File hdrfile = getFile(files, std::regex("hello\\.h"));
-  File iostream = getFile(files, std::regex("iostream"));
-
-  // pp-includes are indexed correcly
+  for (const std::string& snapshot_name : snapshots)
   {
-    std::vector<Include> includes = s.getIncludedFiles(srcfile.id);
-    REQUIRE(includes.size() == 1);
-    REQUIRE(includes.front().includedFileID == hdrfile.id);
+    SnapshotReader s{ snapshot_name };
 
-    includes = s.getIncludedFiles(hdrfile.id);
-    REQUIRE(includes.size() == 1);
-    REQUIRE(includes.front().includedFileID == iostream.id);
-  }
+    std::vector<File> files = s.getFiles();
+    REQUIRE(files.size() >= 2);
+    File srcfile = getFile(files, std::regex("main\\.cpp"));
+    File hdrfile = getFile(files, std::regex("hello\\.h"));
+    File iostream = getFile(files, std::regex("iostream"));
 
-  SymbolRecord stdns = s.getSymbolByName("std");
-  REQUIRE(stdns.kind == SymbolKind::Namespace);
-  SymbolRecord stdcout = s.getSymbolByName("cout");
-  SymbolRecord stdendl = getRecord(s, SymbolRecordFilter().withNameLike("endl(%)").withParent(stdns.id));
+    // pp-includes are indexed correcly
+    {
+      std::vector<Include> includes = s.getIncludedFiles(srcfile.id);
+      REQUIRE(includes.size() == 1);
+      REQUIRE(includes.front().includedFileID == hdrfile.id);
 
-  // references to std symbols are correct
-  {
-    std::vector<SymbolReference> refs = s.findReferences(stdcout.id);
-    REQUIRE(!refs.empty());
-    filterRefs(refs, SymbolRefPattern(stdcout).inFile(hdrfile));
-    REQUIRE(refs.size() == 1);
+      includes = s.getIncludedFiles(hdrfile.id);
+      REQUIRE(includes.size() == 1);
+      REQUIRE(includes.front().includedFileID == iostream.id);
+    }
 
-    refs = s.findReferences(stdendl.id);
-    REQUIRE(!refs.empty());
-    filterRefs(refs, SymbolRefPattern(stdendl).inFile(hdrfile));
-    REQUIRE(refs.size() == 1);
+    SymbolRecord stdns = s.getSymbolByName("std");
+    REQUIRE(stdns.kind == SymbolKind::Namespace);
+    SymbolRecord stdcout = s.getSymbolByName("cout");
+    SymbolRecord stdendl = getRecord(s, SymbolRecordFilter().withNameLike("endl(%)").withParent(stdns.id));
+
+    // references to std symbols are correct
+    {
+      std::vector<SymbolReference> refs = s.findReferences(stdcout.id);
+      REQUIRE(!refs.empty());
+      filterRefs(refs, SymbolRefPattern(stdcout).inFile(hdrfile));
+      REQUIRE(refs.size() == 1);
+
+      refs = s.findReferences(stdendl.id);
+      REQUIRE(!refs.empty());
+      filterRefs(refs, SymbolRefPattern(stdendl).inFile(hdrfile));
+      REQUIRE(refs.size() == 1);
+    }
   }
 }
